726345a1348f2ac45e5c44388e7a6926453b4811
Laura Chaur <nchaur@gmail.com>
2022-04-29 13:55:48 +0200

    initial commit

---
+++ src/app/shared/FREEBIES.js
@@ -0,0 +1,35 @@
+import freebie1 from "../img/freebie1.png";
+import freebie2 from "../img/freebie2.png";
+import freebie3 from "../img/freebie3.png";
+
+export const FREEBIES = [
+  {
+    id: 0,
+    name: "Bootstrap Outfitters",
+    image: freebie1,
+    featured: false,
+    description: "Bootstrap Outfitters supplies you with the gear you need at prices you can't beat.",
+  },
+  {
+    id: 1,
+    name: "Git Out Expeditions",
+    image: freebie2,
+    featured: false,
+    description: "Join Git Out Expeditions to explore new horizons with a group of other adventurers!",
+  },
+  {
+    id: 2,
+    name: "Mongo Fly Shop",
+    image: freebie3,
+    featured: false,
+    description: "Need a new fishing pole, a tacklebox, or flies of all kinds? Stop by Mongo Fly Shop.",
+  },
+  {
+    id: 3,
+    name: "Node Outdoor Apparel",
+    image: freebie3,
+    featured: true,
+    description:
+      "From polar fleeces to swimsuits, hiking boots to waders, a visit to Node will be sure to get you covered.",
+  },
+];
---
+++ src/app/shared/PRODUCTS.js
@@ -0,0 +1,41 @@
+import shop1 from "../img/shop1.png";
+import shop2 from "../img/shop2.png";
+import shop3 from "../img/shop3.png";
+import shop4 from "../img/shop4.png";
+
+export const PRODUCTS = [
+  {
+    id: 0,
+    name: "React Lake Campground",
+    image: shop1,
+    elevation: 1233,
+    featured: false,
+    description:
+      "Nestled in the foothills of the Chrome Mountains, this campground on the shores of the pristine React Lake is a favorite for fly fishers.",
+  },
+  {
+    id: 1,
+    name: "Chrome River Campground ",
+    image: shop2,
+    elevation: 877,
+    featured: false,
+    description:
+      "Spend a few sunny days and starry nights beneath a canopy of old-growth firs at this enchanting spot by the Chrome River.",
+  },
+  {
+    id: 2,
+    name: "Breadcrumb Trail Campground",
+    image: shop3,
+    elevation: 2901,
+    featured: false,
+    description: "Let NuCamp be your guide to this off-the-beaten-path, hike-in-only campground.",
+  },
+  {
+    id: 3,
+    name: "Redux Woods Campground",
+    image: shop4,
+    elevation: 42,
+    featured: true,
+    description: "You'll never want to leave this hidden gem, deep within the lush Redux Woods.",
+  },
+];
